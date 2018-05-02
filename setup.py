667d00e558db817a9ce76c26c2eae6c51f8a3b0c
U-AD\clindsay <clindsay@edinb751369.ad.cirrus.com>
2018-05-02 12:52:33 +0100

    pip install https://*.master.zip was failing, now successfully works with 'pip install https://github.com/theirishkiwi/pyhtgen/archive/master.zip'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Setup script for pyhtgen
 # CHANGELOG
 # 2009-03-02 v0.01: - 1st version
 # 2010-05-25 v0.02: - renamed HTML.py to pyhtgen, package instead of module
-# 2018-05-20 v0.5 - 
+# 2018-05-20 v0.05.1: - pip install https://*.master.zip was failing, now successfully works with 'pip install https://github.com/theirishkiwi/pyhtgen/archive/master.zip'
 VERSION = '0.5'
 
 long_description = ''' A Python module to easily generate HTML code (tables, lists, ...).
@@ -17,7 +17,7 @@ from distutils.core import setup
 import HTML
 
 setup(name='pyhtgen',
-      version='0.4',
+      version='0.5.1*',
       description='A Python module to easily generate HTML code (tables, lists, ...)',
       author='Philippe Lagadec',
       author_email='decalage (a) laposte.net',
